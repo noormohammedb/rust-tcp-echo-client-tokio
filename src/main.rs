bf2eb6a874bdfc4bbf8515f71588d4b3bc25e725
Noor Mohammed B <noormohammedb@protonmail.com>
2022-07-06 22:10:10 +0530

    Tokio integration

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
-use std::io::prelude::*;
-use std::net::TcpStream;
+use tokio::io::AsyncReadExt;
+use tokio::{io::AsyncWriteExt, net::TcpStream};
 
 const ECHO_SERVER_ADDRESS: &str = "localhost:1234";
 
-fn main() {
+#[tokio::main]
+async fn main() {
     // connection
     println!("connecting to tcp://{}", ECHO_SERVER_ADDRESS);
 
-    if let Ok(mut tcp_stream_connect) = TcpStream::connect(ECHO_SERVER_ADDRESS) {
+    if let Ok(mut tcp_stream_connect) = TcpStream::connect(ECHO_SERVER_ADDRESS).await {
         println!("connected: {:?}", tcp_stream_connect);
         println!(
             "connected to echo server {}:{}",
@@ -16,12 +17,12 @@ fn main() {
         );
 
         let message = "Hello World!";
-        let _ = tcp_stream_connect.write(message.as_bytes());
-        let _ = tcp_stream_connect.flush();
+        let _ = tcp_stream_connect.write_all(message.as_bytes()).await;
+        // let _ = tcp_stream_connect.flush();
 
         // read the result
         let mut buffer = [0; 1024];
-        tcp_stream_connect.read(&mut buffer).unwrap();
+        tcp_stream_connect.read(&mut buffer).await.unwrap();
         let message = String::from_utf8_lossy(&buffer);
 
         println!("received: {}", message);
