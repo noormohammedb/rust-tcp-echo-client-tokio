114722b9e7a6063e36d97fa534f27def691d358e
Noor Mohammed B <noormohammedb@protonmail.com>
2022-07-06 21:10:53 +0530

    TCP Connected and passed a message

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use std::io::prelude::*;
+use std::net::TcpStream;
+
+const ECHO_SERVER_ADDRESS: &str = "localhost:1234";
+
 fn main() {
-    println!("Hello, world!");
+    // connection
+    println!("connecting to tcp://{}", ECHO_SERVER_ADDRESS);
+
+    if let Ok(mut tcp_stream_connect) = TcpStream::connect(ECHO_SERVER_ADDRESS) {
+        println!("connected: {:?}", tcp_stream_connect);
+        println!(
+            "connected to echo server {}:{}",
+            tcp_stream_connect.local_addr().unwrap().ip(),
+            tcp_stream_connect.local_addr().unwrap().port(),
+        );
+
+        let message = "Hello World!";
+        let _ = tcp_stream_connect.write(message.as_bytes());
+        let _ = tcp_stream_connect.flush();
+
+        // read the result
+        let mut buffer = [0; 1024];
+        let len = tcp_stream_connect.read(&mut buffer).unwrap();
+    } else {
+        println!("failed to connect");
+        // return;
+    }
 }
